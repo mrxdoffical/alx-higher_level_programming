598930ebac027a4e6b73fa98f37aea6ab4920396
mrxdoffical <berogergesg@gmail.com>
2024-01-24 11:30:37 +0200

    adde tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square module"""
+class Square:
+	"""Square definition"""
+	def __init__(self, size):
+		"""_summary_
+
+		Args:
+			size (_type_): _description_
+		"""
+		self.__size = size
