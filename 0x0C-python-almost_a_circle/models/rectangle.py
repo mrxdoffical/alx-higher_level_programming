7c0a96da257817f2ac8e3bdd52d0b6cd6dbf5a1b
mrxdoffical <berogergesg@gmail.com>
2024-02-12 13:34:48 +0200

    added tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,35 +11,43 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
     @property
     def width(self):
         '''width of the rectangle'''
         return self.__width
+
     @width.setter
     def width(self, value):
         self.__width = value
+
     @property
     def height(self):
         '''height of the rectangle'''
         return self.__height
+
     @height.setter
     def height(self, value):
         self.__height = value
+
     @property
     def x(self):
         '''x position of the rectangle'''
         return self.__x
+
     @x.setter
     def x(self, value):
         self.__x = value
+
     @property
     def y(self):
         '''x position of the rectangle'''
         return self.__y
+
     @y.setter
     def y(self, value):
         self.__y = value
-    
+
     def validate_intger(self, name, value, eq=True):
         '''method for validation'''
         if type(value) is not int:
@@ -48,17 +56,39 @@ class Rectangle(Base):
             raise ValueError("{} must be >= 0".format(name))
         elif eq and value <= 0:
             raise ValueError("{} must be > 0".format(name))
+
     def area(self):
         '''computes area of this rectangle'''
         return self.width * self.height
-    
+
     def display(self):
         '''print string representation'''
         s = '\n' * self.y + \
             (' ' * self.x + self.width + '\n') * self.height
         print(s, end='')
+
     def __str__(self):
         '''returns a string representation of rectangle'''
         return '[{}] ({}) {}/{} - {}/{}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width,
                    self.height)
+
+    def __update(self, id=None, x=None, y=None, width=None, height=None):
+        if id is not None:
+            self.id = id
+        if x is not None:
+            self.x = x
+        if height is not None:
+            self.height = height
+        if y is not None:
+            self.y = y
+        if width is not None:
+            self.width = width
+
+    def update(self, *args, **kwargs):
+        '''update instance with no-keyword & keyword arguments'''
+        #print(args, kwargs)
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
