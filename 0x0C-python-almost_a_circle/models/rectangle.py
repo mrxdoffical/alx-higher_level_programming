65302d33287c1772d8b30a4ea49d363d742a27a6
mrxdoffical <berogergesg@gmail.com>
2024-02-12 13:12:05 +0200

    added tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -39,3 +39,12 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         self.__y = value
+    
+    def validate_intger(self, name, value, eq=True):
+        '''method for validation'''
+        if type(value) is not int:
+            raise ValueError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
