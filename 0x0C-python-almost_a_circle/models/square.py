963d6d00a19dc68c5947a7e25f037d429bd480fd
mrxdoffical <berogergesg@gmail.com>
2024-02-12 13:54:39 +0200

    added tasks

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,3 +40,8 @@ class square(Rectangle):
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
+
+    def to_dictionary(self):
+        '''return a dictionary representation of this class'''
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
