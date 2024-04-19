20fa9c25a7a179a9a6844f272051bbd594da729a
mrxdoffical <berogergesg@gmail.com>
2024-04-20 00:23:00 +0200

    added task0 as a test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,18 +10,16 @@ def list_states(username, password, dbname):
     """
     Connects to a MySQL database and prints all states sorted by states.id.
     """
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=dbname)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
-
-if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        dbname = sys.argv[3]
-        list_states(username, password, dbname)
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=dbname)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+    except MySQLdb.DatabaseError as e:
+        print(f"Database error: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
