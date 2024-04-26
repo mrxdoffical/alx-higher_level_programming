e78776f6d6c6460445fad8b30bf9d7511b6f8e57
mrxdoffical <berogergesg@gmail.com>
2024-04-26 20:00:45 +0300

    added tasks

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Check if an argument is provided
+if [ $# -eq 0 ]
+then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# Use curl to fetch the HTTP status code
+status_code=$(curl -o /dev/null -s -w "%{http_code}" "$1")
+
+# Display the status code
+echo $status_code
