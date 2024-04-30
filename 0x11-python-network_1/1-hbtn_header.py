f7f697419c44e6adf70f9dbdf64dc9664aa295e1
mrxdoffical <berogergesg@gmail.com>
2024-04-30 21:04:32 +0300

    added tasks

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,11 @@
-#!/usr/bin/python3
-"""
-This module provides functionality to fetch and display the 'X-Request-Id' header value from a response obtained by sending a request to a specified URL.
-It uses the urllib.request module to handle the HTTP request and sys module to read the URL from command line arguments.
-"""
-
-import urllib.request
 import sys
+import urllib.request
+
 
 def fetch_request_id(url):
     """
-    Sends a request to the given URL and retrieves the 'X-Request-Id' header from the response.
+    Sends a request to the given URL and retrieves the 'X-Request-Id' header 
+    from the response.
 
     Parameters:
     url (str): The URL to which the request will be sent.
@@ -20,6 +16,7 @@ def fetch_request_id(url):
     with urllib.request.urlopen(url) as response:
         request_id = response.headers.get('X-Request-Id')
         return request_id
+
 
 if __name__ == "__main__":
     # Assume the URL is passed as the first command line argument
