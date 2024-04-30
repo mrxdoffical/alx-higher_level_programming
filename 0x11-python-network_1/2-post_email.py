2f32520a87d00ff2dc8b62faada30b068a285403
mrxdoffical <berogergesg@gmail.com>
2024-04-30 22:19:32 +0300

    added tasks

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+import sys
+from urllib import request, parse
+
+def send_post_request(url, email):
+    # Data to be sent in the POST request
+    data = parse.urlencode({'email': email}).encode()
+
+    # Create a request object
+    req = request.Request(url, data=data)  # 'data' automatically makes it a POST request
+
+    # Send the request and read the response
+    with request.urlopen(req) as response:
+        response_body = response.read().decode('utf-8')
+        print("Your email is: ", response_body)
+
+if __name__ == "__main__":
+    # Command line arguments
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Call the function with the URL and email
+    send_post_request(url, email)
