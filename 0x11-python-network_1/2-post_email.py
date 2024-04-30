846c537cc99c9e866551ada212d7de3b8f20b6cf
mrxdoffical <berogergesg@gmail.com>
2024-04-30 22:30:38 +0300

    added tasks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,44 +1,19 @@
 #!/usr/bin/python3
-import sys
-from urllib import request, parse
-
 """
-Module for sending POST requests with an email parameter.
+Script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8).
 """
+from sys import argv
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 
-def send_post_request(url, email):
-    """
-    Sends a POST request to the specified URL with the given email.
-
-    Parameters:
-        url (str): The URL to which the POST request is sent.
-        email (str): The email address to be sent as a part of the POST request.
-
-    Returns:
-        None
-
-    Prints:
-        The body of the response, decoded in utf-8.
-    """
-    # Data to be sent in the POST request
-    data = parse.urlencode({'email': email}).encode()
-
-    # Create a request object
-    req = request.Request(url, data=data)  # 'data' automatically makes it a POST request
-
-    # Send the request and read the response
-    with request.urlopen(req) as response:
-        response_body = response.read().decode('utf-8')
-        print(response_body)
 
 if __name__ == "__main__":
-    """
-    Main execution point of the script. Takes a URL and an email from the command line,
-    sends a POST request to the URL with the email as a parameter, and prints the response.
-    """
-    # Command line arguments
-    url = sys.argv[1]
-    email = sys.argv[2]
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = urlencode(value).encode("ascii")
+    req = Request(url, data)
 
-    # Call the function with the URL and email
-    send_post_request(url, email)
+    with urlopen(req) as response:
+        print(response.read().decode("utf-8", "replace"))
