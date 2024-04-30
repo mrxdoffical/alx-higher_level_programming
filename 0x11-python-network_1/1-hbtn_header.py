9afbe5bf8ab1ad7d4ed19c3b11be72238607b5eb
mrxdoffical <berogergesg@gmail.com>
2024-04-30 21:06:21 +0300

    added tasks

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
+import urllib
 import sys
 import urllib.request
 
-
 def fetch_request_id(url):
-    """
-    Sends a request to the given URL and retrieves the 'X-Request-Id' header 
-    from the response.
-
-    Parameters:
-    url (str): The URL to which the request will be sent.
-
-    Returns:
-    str: The value of the 'X-Request-Id' header.
-    """
     with urllib.request.urlopen(url) as response:
+        # Retrieve the X-Request-Id header value
         request_id = response.headers.get('X-Request-Id')
         return request_id
-
 
 if __name__ == "__main__":
     # Assume the URL is passed as the first command line argument
