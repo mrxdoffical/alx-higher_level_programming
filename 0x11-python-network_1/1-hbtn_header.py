d2090f7b250ee2993af878aa91fe59cbeaf5d184
mrxdoffical <berogergesg@gmail.com>
2024-04-30 22:26:42 +0300

    added tasks

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
+"""
+Script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response.
 
-"""fetching the x-request-id"""
+Usage: ./1-hbtn_header.py <URL>
+"""
+from sys import argv
+from urllib.request import Request, urlopen
 
-
-def fetch_request_id(url):
-    """
-    Fetch the 'X-Request-Id' from the HTTP response headers of a given URL.
-
-    Args:
-        url (str): The URL from which to fetch the HTTP response.
-
-    Returns:
-        str: The value of the 'X-Request-Id' header, if present. None otherwise.
-    """
-    with urllib.request.urlopen(url) as response:
-        request_id = response.headers.get('X-Request-Id')
-        return request_id
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    request_id = fetch_request_id(url)
-    print(request_id)
+    url = argv[1]
+    req = Request(url)
+
+    with urlopen(req) as response:
+        print(dict(response.headers).get("X-Request-Id"))
