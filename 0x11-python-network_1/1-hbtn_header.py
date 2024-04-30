a9d52688dd68476bb55dd0a5282708cab4191e3a
mrxdoffical <berogergesg@gmail.com>
2024-04-30 22:27:33 +0300

    added tasks

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL, sends a request to the URL and displays
-the value of the X-Request-Id variable found in the header of the response.
+Script that takes in a URL
 
 Usage: ./1-hbtn_header.py <URL>
 """
